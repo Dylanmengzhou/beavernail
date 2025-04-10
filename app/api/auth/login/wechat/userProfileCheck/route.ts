363790e3ac76654d2c010b0cb18a02916822017a
bimengzhou <dylanmengzhou@gmail.com>
2025-04-10 23:22:43 +0900

    version 1.5.3: 增加微信用户更改个人信息api

---
+++ app/api/auth/login/wechat/userProfileCheck/route.ts
@@ -0,0 +1,28 @@
+import { NextRequest, NextResponse } from "next/server";
+import prisma from "@/lib/prisma";
+
+export async function POST(req: NextRequest) {
+    const { userId } = await req.json();
+    console.log("账号检查，获得的userId: ", userId)
+    const user = await prisma.user.findUnique({
+        where: { id: userId },
+    })
+
+    if (!user) {
+        return NextResponse.json(
+            { success: false, message: "用户不存在" },
+            { status: 400 }
+        );
+    }
+
+    if (!user.name || !user.email) {
+        return NextResponse.json(
+            { success: false, message: "用户信息不完整" },
+            { status: 400 }
+        );
+    }
+    return NextResponse.json({
+        success: true,
+        message: "用户信息完整",
+    });
+}
---
+++ app/api/auth/login/wechat/userProfileUpdate/route.ts
@@ -0,0 +1,32 @@
+import { NextRequest, NextResponse } from "next/server";
+import prisma from "@/lib/prisma";
+
+export async function POST(req: NextRequest) {
+    const { userId, nickName, contactNumber } = await req.json();
+
+    console.log("userId: ", userId)
+    console.log("nickName: ", nickName)
+    console.log("contactNumber: ", contactNumber)
+    const user = await prisma.user.findUnique({
+        where: { id: userId },
+    })
+
+    if (!user) {
+        return NextResponse.json(
+            { success: false, message: "用户不存在" },
+            { status: 400 }
+        );
+    }
+
+    await prisma.user.update({
+        where: { id: userId },
+        data: {
+            name: nickName,
+            email: contactNumber,
+        },
+    })
+    return NextResponse.json({
+        success: true,
+        message: "用户信息更新成功",
+    });
+}
